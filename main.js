69c9eb234347fff2fc68fe993bad3a9922eb7698
Davismeru <davismeru911@gmail.com>
2022-08-15 17:39:41 +0300

    added,save input in local storage, see all profiles, see individual profiles by fetching data saved in local storage functionalities

--- main.js
+++ main.js
@@ -1,92 +1,125 @@
-// all survey question points
+// all survey questions array
 const questionaires = ["first name", "surname", "Id number", "age", "county", "gender", "highest education level", "hobby", "profession", "favorite movie"]
+const wrapper = document.querySelector('.wrapper')
+const allBtns = wrapper.querySelectorAll('.btn')
+const prevBtn = wrapper.querySelector('.prev')
+const nextBtn = wrapper.querySelector('.next')
+const specifics = wrapper.querySelector('#specifics')
+const textInput = wrapper.querySelector('.text-input')
+const notificationPopup = wrapper.querySelector('.notification-popup')
+const profilesSlide = wrapper.querySelector('.profiles-slide')
+const profilesList = profilesSlide.querySelector('.profiles-list')
 
-const specifics = document.querySelector('#specifics')
-const btns = document.querySelectorAll('.btn')
-const textInput = document.querySelector('.text-input')
-const prevBtn = document.querySelector('.prev')
-const nextBtn = document.querySelector('.next')
-const notificationPopup = document.querySelector('.notification-popup')
-const profilesSlide = document.querySelector('.profiles-slide')
-const profilesList = document.querySelector('.profiles-list')
-prevBtn.style.display = "none"
-let tempArr = []
+prevBtn.style.display = "none" //hide previous button by default and only diplay if count is greater than 0
+
 let count = 0
-let emptyString = ''
-btns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        const clickedBtn = e.currentTarget.getAttribute('value')
-        // next button functionality
-        if(clickedBtn === "Next") {
-            if(textInput.value) { //only show next question if the current field is not empty
+let tempArr = []
+allBtns.forEach(btn => {
+    btn.addEventListener('click', (e)=> {
+        const clickedBtn  = e.currentTarget.getAttribute("value")
+        if(clickedBtn == "Next") {
+            if(textInput.value != "") {
                 count++
-                tempArr.push(`"${textInput.value}"`)
-                textInput.value = ''
-            } else { //show this alert message if field is empty
+                tempArr.push(textInput.value)
+                textInput.value = "" // clear the current value of the input
+            } else {
                 alert("please fill this field")
             }
-        }  else if(clickedBtn === "Previous") { // previous button functionality
+        } else if(clickedBtn == "Previous") {
             count--
-            textInput.value = tempArr[count]
+            textInput.value = `${tempArr[count]}`
             tempArr.pop()
-        } else if(clickedBtn ==="Submit"){    //submit button functionality
-            tempArr.push(`"${textInput.value}"`)
-            localStorage.setItem(`${tempArr[2]}`, tempArr)
+        } else if(clickedBtn == "Submit") {
+            tempArr.push(textInput.value) //push the last value into the tempArr array
+            localStorage.setItem(`${tempArr[2]}`, JSON.stringify(tempArr))
             tempArr = []
+            textInput.value = ''
             notificationPopup.classList.add('show-popup')
             setTimeout(()=> {
                 notificationPopup.classList.remove('show-popup')
             }, 2000)
             count = 0
-            textInput.value = ''
-        } else { //see profiles button functionality
-            profilesSlide.classList.add('show-profiles-slide')
+        } else if(clickedBtn == "Profiles") {
+            profilesSlide.classList.add('show-profiles-slide') //show profiles slide when profiles button is clicked
         }
-        // hide previous button at the beginning of the survey
-        count > 0 ? prevBtn.style.display = "inline" : prevBtn.style.display = "none"
-        // show submit button at the end of the survey
+        // display submit button once we are in the last questionaire    
         count == questionaires.length-1 ? nextBtn.setAttribute("value", "Submit") : nextBtn.setAttribute("value", "Next")
+        // display previous button if count is greater than 0
+        count > 0 ? prevBtn.style.display = "inline" : prevBtn.style.display = "none"
         specifics.textContent = questionaires[count]
-        textInput.setAttribute('placeholder', `${questionaires[count]}`)
+        textInput.setAttribute("placeholder", `${questionaires[count]}`)
     })
 })
-
-let keys = Object.keys(localStorage)
-let profilesArr = []
-keys.map((key) => {
-    const fetchedProfiles = `[${localStorage.getItem(key)}]`
-    profilesArr.push(fetchedProfiles)
-    profilesArr.forEach(profile => {
-        let toArr = JSON.parse(profile)
-        console.log(profile);
-        profilesList.innerHTML += `
-        <div class="list-content">
-            <div class="name">
-                <p>${toArr[0] + " " + toArr[1]}</p>
-            </div>
-
-            <div class="id-number">
-                <p>${JSON.parse(toArr[2])}</p>
-            </div>
-
-            <div class="profile-link">
-                <p class="profile-link">see profile</p>
-            </div>
-        </div>
-        `
-    })
-})
-
 
 // hide profiles slide when home button is clicked
-const homeBtn = document.querySelector('.fa-home')
+const homeBtn = profilesSlide.querySelector('.fa-home')
 homeBtn.addEventListener('click', ()=> {
     profilesSlide.classList.remove('show-profiles-slide')
 })
 
-// display all profiles in the profiles slide
+// refresh button functionality
+const refreshBtn = profilesSlide.querySelector('.fa-refresh')
+refreshBtn.addEventListener('click', ()=> {
+    location.reload()
+})
 
+// display items in the profiles slide from the local storage
+const keys = Object.keys(localStorage) // get all items stored in the local storage by their key (i.e id number)
+keys.forEach(key => {
+    const fetchedSurveys = JSON.parse(localStorage.getItem(key)) //get all items in the local storage and convert them into arrays
+    profilesList.innerHTML += `
+        <div class="list-content">
+            <div class="name">
+                <p>${fetchedSurveys[0] + " " + fetchedSurveys[1]}</p>
+            </div>
 
+            <div class="id-number">
+                <p>${fetchedSurveys[2]}</p>
+            </div>
+
+            <div class="profile-link">
+                <p class="single-link">see profile</p>
+            </div>
+        </div>
+        `
+})
+
+// see individual profile
+const allListContents = profilesSlide.querySelectorAll('.list-content')
+const profilePopup = profilesSlide.querySelector('.single-profile-popup')
+const popupInfo = profilePopup.querySelector('.popup-info')
+allListContents.forEach(list => {
+    const profileLink = list.querySelector('.single-link')
+    profileLink.addEventListener('click', (e)=> {
+        profilePopup.classList.add('show-profile-popup')
+        const getId = e.currentTarget.parentElement.parentElement.querySelector('.id-number p').textContent
+        const fetchedProfiles = JSON.parse(localStorage.getItem(getId))
+        popupInfo.innerHTML = `
+        <div class="col">
+            <h6>First name: <span>${fetchedProfiles[0]}</span></h6>
+            <h6>Surname: <span>${fetchedProfiles[1]}</span></h6>
+            <h6>Id number: <span>${fetchedProfiles[2]}</span></h6>
+            <h6>Age: <span>${fetchedProfiles[3]}</span></h6>
+            <h6>county: <span>${fetchedProfiles[4]}</span></h6>
+        </div>
+
+        <div class="col">
+            <h6>Gender: <span>${fetchedProfiles[5]}</span></h6>
+            <h6>Education: <span>${fetchedProfiles[6]}</span></h6>
+            <h6>Hobby: <span>${fetchedProfiles[7]}</span></h6>
+            <h6>Profession: <span>${fetchedProfiles[8]}</span></h6>
+            <h6>Favorite movie: <span>${fetchedProfiles[9]}</span></h6>
+        </div>`
+    })
+})
+
+// close popup
+const closeBtn = profilesSlide.querySelector('.fa-x')
+closeBtn.addEventListener('click', ()=> {
+    profilePopup.classList.remove('show-profile-popup')
+})
+
+// clear button functionality
 const clear = document.querySelector('.clear')
 clear.addEventListener('click', ()=> {
     localStorage.clear()
